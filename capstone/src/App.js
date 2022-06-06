eb5d8a82b9354f0c83e757f0269f8c662d428722
Laxmikant Sambhare <lsambhare42@gmail.com>
2022-06-06 14:42:28 +0530

    source

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,56 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Adoption from './Components/Adoption/Adoption';
+import Home from './Components/Home/Home'
+import Rescue from './Components/Rescue/Rescue';
+import Store from './Components/Store/Store';
+import Veternari from './Components/Veternari/Veternari';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+     <BrowserRouter>
+     <Routes>
+     <Route exact path="/" element={<Home />} />
+     <Route exact path="/rescue" element={<Rescue />} />
+     <Route exact path="/adoption" element={<Adoption />} />
+     <Route exact path="/store" element={<Store />} />
+     <Route exact path="/vet" element={<Veternari />} />
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ capstone/src/Components/Veternari/Veternari.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Veternari = () => {
+  return (
+    <div>Veternari</div>
+  )
+}
+
+export default Veternari
