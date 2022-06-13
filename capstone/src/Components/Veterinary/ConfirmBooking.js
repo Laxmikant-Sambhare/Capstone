68db4b46f0cd04ed2eb28099f5fe694438fe8846
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-13 19:57:00 +0530

    component

--- capstone/src/Components/Veterinary/ConfirmBooking.js
+++ capstone/src/Components/Veterinary/ConfirmBooking.js
@@ -1,13 +1,36 @@
-import React from "react";
-import { Booking, Heading, Payment, Text1 } from "./ConfirmBooking.styling";
+import React, { useState } from "react";
+import {
+  Booking,
+  Heading,
+  Payment,
+  Text1,
+  Container,
+} from "./ConfirmBooking.styling";
+import StripeCheckout from "react-stripe-checkout";
+
 function ConfirmBooking() {
+  function handleToken(token, addresses) {
+    console.log({ token, addresses });
+  }
   return (
     <Booking>
       <Heading>Payment Options</Heading>
-      <Payment>
-        <Text1>Card</Text1>
-        <Text1>G-pay</Text1>
-      </Payment>
+      <Container>
+        <Payment>
+          <Text1>Card Payment</Text1>
+          <StripeCheckout
+            stripeKey="pk_test_51KuyaPSCQaVx7HBe8OxRf2sNJR6frOJUsRuVFJzPvui7Ni4icPjxO5u1YjNTWAwFtK9xxxQS03dMb6aetPKBxzSP004S3AwqfR"
+            token={handleToken}
+            billingAddress
+            shippingAddress
+            amount={100} //*100 for converting from dollar to blaah
+            name="Service Charge"
+          />
+          <br></br>
+          <br></br>
+          <Text1>G-Pay</Text1>
+        </Payment>
+      </Container>
     </Booking>
   );
 }
