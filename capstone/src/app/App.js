5d9d9715d1b844607dfa00435985d765aa898b3c
Laxmikant Sambhare <lsambhare42@gmail.com>
2022-06-13 11:45:50 +0530

    added login

--- capstone/src/app/App.js
+++ capstone/src/app/App.js
@@ -14,23 +14,6 @@ import ConfirmBooking from "../Components/Veterinary/ConfirmBooking";
 import Productdiscription from "../Components/Store/Productdiscription";
 import Signup from "../Components/Signup/Signup";
 import Login from "../Components/Signup/Login";
-<<<<<<< HEAD
-import { UserAuthContextProvider } from "../context/UserAuthContext";
-import ProtectedRoute from "../Components/Signup/ProtectedRoute";
-function App() {
-  return (
-    <div className="App">
-      <UserAuthContextProvider>
-        <Navbar />
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-          
-            <Route exact path="/rescue" element={<Rescue />} />
-            <Route exact path="/adoption" element={<Adoption />} />
-            <Route exact path="/adoption/:id" element={<DogDescription />} />
-
-=======
 
 // import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider } from "../context/UserAuthContext";
@@ -49,7 +32,6 @@ function App() {
             <Route exact path="/adoption" element={<Adoption />} />
             <Route exact path="/adoption/:id" element={<DogDescription />} />
 
->>>>>>> dde661e1d538c8d4bcfebf66302a22196e1d7ace
             <Route exact path="/store" element={<Store />} />
             <Route exact path="/store/:id" element={<Productlisting />} />
             <Route
@@ -58,13 +40,6 @@ function App() {
               element={<Productdiscription />}
             />
             <Route exact path="/vet" element={<Veterinary />} />
-<<<<<<< HEAD
-
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/login" element={<Login />} />
-
-=======
->>>>>>> dde661e1d538c8d4bcfebf66302a22196e1d7ace
             <Route exact path="/store/:id" element={<Productlisting />} />
             <Route exact path="/vet/:id" element={<Booking />} />
             <Route exact path="/confirmBooking" element={<ConfirmBooking />} />
