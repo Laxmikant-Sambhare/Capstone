fdbbc0e6fe0581e3b95d6d9e230a3a31907c76f2
Laxmikant Sambhare <lsambhare42@gmail.com>
2022-06-15 21:01:43 +0530

    addtocart

--- capstone/src/Components/Store/Cart.js
+++ capstone/src/Components/Store/Cart.js
@@ -98,7 +98,7 @@ const Cart = () => {
             <div className="cart-checkout">
               <div className="subtotal">
                 <span>Subtotal</span>
-                <span className="amount">₹{cart.cartTotalAmount}</span>
+                <span className="amount"> ₹{cart.cartTotalAmount}</span>
               </div>
               <p>Taxes and shipping calculated at checkout</p>
               <button>Check out</button>
@@ -117,7 +117,7 @@ const Cart = () => {
                       d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
                     />
                   </svg>
-                  <span>Continue Shopping</span>
+                  <h3>Continue Shopping</h3>
                 </Link>
               </div>
             </div>
