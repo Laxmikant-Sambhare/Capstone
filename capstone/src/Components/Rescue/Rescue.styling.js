1374b33d10bd8da69affb55b28737c51468ee481
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-15 19:07:53 +0530

    Rescue.styling.js

--- capstone/src/Components/Rescue/Rescue.styling.js
+++ capstone/src/Components/Rescue/Rescue.styling.js
@@ -1,69 +1,54 @@
 import styled from "styled-components";
 
-export const Header = styled.div`
-  background-color: black;
-  width: 100%;
-  height: 100px;
-  color: white;
-  font-size:50px;
-  font-family: "Jost", sans-serif;  
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-// export const Rescue = styled.div``;
-export const Image = styled.div`
-  margin-top: 5%;
-  margin-left: 40%;
-
-  > img {
-    width: 30%;
-    height: 30%;
-    border-radius: 50%;
-  }
-`;
 export const Form = styled.div`
-  width: 90%;
-  height: 50%;
-  margin-top: 3%;
-  margin-left: 4%;
-  padding: 2%;
+  width: 35%;
+  height: 30%;
+  
+  border-radius:15px;
+  margin-top: -30%;
+  margin-left:32%;
+  /* margin-right: 40%;  */
+  /* padding: 2%; */
+  background-color:#ffd301;
+  
 `;
 export const Container1 = styled.div`
   margin-left: 40%;
+  margin-top: -40%;
 
   > input {
-    width:30%;
+    width: 50%;
+    margin-bottom: 5%;
   }
 `;
 export const Container2 = styled.div`
   margin-left: 40%;
   > input {
-    width:29.5%;
-
+    width: 50.5%;
+    margin-bottom: 5%;
   }
 `;
 export const Container3 = styled.div`
   margin-left: 40%;
   > input {
-    width:31%
-
+    margin-bottom: 5%;
+    width: 50%;
   }
 `;
 export const Container4 = styled.div`
   margin-left: 40%;
-  >input{
-    width:30%;
-
+  > input {
+    width: 50%;
+    margin-bottom: 5%;
   }
 `;
 export const Container5 = styled.div`
   margin-left: 40%;
+  margin-bottom:5%;
 `;
 export const Button = styled.button`
-  width: 15%;
+  width: 50%;
   padding: 1%;
-  margin-left:40%;
-
+  margin-left: 40%;
+  margin-top:-50px;
 `;
-
