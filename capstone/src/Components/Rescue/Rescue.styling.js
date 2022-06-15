c9a9a2c7c805b58abb2c2dc62f48098978c1e35d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-15 22:25:59 +0530

    Rescue.styling.js

--- capstone/src/Components/Rescue/Rescue.styling.js
+++ capstone/src/Components/Rescue/Rescue.styling.js
@@ -4,7 +4,7 @@ export const Form = styled.div`
   width: 35%;
   height: 30%;
   
-  border-radius:15px;
+  border-radius:14px;
   margin-top: -30%;
   margin-left:32%;
   /* margin-right: 40%;  */
@@ -16,6 +16,7 @@ export const Container1 = styled.div`
   border-radius:10px;
   margin-left: 30%;
   margin-top: -40%;
+  font-size:13.5px;
 
   > input {
     width: 50%;
@@ -25,6 +26,8 @@ export const Container1 = styled.div`
 export const Container2 = styled.div`
   border-radius:10px;
   margin-left: 30%;
+  font-size:13.5px;
+
 
   > input {
     width: 48%;
@@ -34,6 +37,8 @@ export const Container2 = styled.div`
 export const Container3 = styled.div`
   border-radius:10px;
   margin-left: 30%;
+  font-size:13.5px;
+
   > input {
     margin-bottom: 5%;
     width: 51%;
@@ -42,6 +47,8 @@ export const Container3 = styled.div`
 export const Container4 = styled.div`
   border-radius:10px;
   margin-left: 30%;
+  font-size:13.5px;
+
   > input {
     width: 55%;
     margin-bottom: 5%;
@@ -51,6 +58,8 @@ export const Container5 = styled.div`
   border-radius:10px;
   margin-left: 40%;
   margin-bottom:5%;
+  font-size:13.5px;
+
 `;
 export const Button = styled.button`
   width: 11%;
@@ -59,4 +68,5 @@ export const Button = styled.button`
   margin-top:-50px;
   /* background-color:black; */
   border-radius:8px;
+  
 `;
