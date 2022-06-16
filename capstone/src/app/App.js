7188052ded509c677d96c5e2296aef63f9b6cd0d
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-16 14:24:33 +0530

    source

--- capstone/src/app/App.js
+++ capstone/src/app/App.js
@@ -17,7 +17,7 @@ import Login from "../Components/Signup/Login";
 import { ToastContainer } from "react-toastify";
 // import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider } from "../context/UserAuthContext";
-import Cart from "../Components/Store/Cart"
+import Cart from "../Components/Store/Cart";
 import Footer from "../Components/Footer/Footer";
 function App() {
   return (
@@ -25,11 +25,11 @@ function App() {
       <Navbar />
       <UserAuthContextProvider>
         <BrowserRouter>
-        <ToastContainer/>
+          <ToastContainer />
           <Routes>
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/login" element={<Login />} />
-            <Route exact path="/Cart" element={<Cart/>} />
+            <Route exact path="/Cart" element={<Cart />} />
             <Route exact path="/" element={<Home />} />
             <Route exact path="/rescue" element={<Rescue />} />
             <Route exact path="/adoption" element={<Adoption />} />
@@ -44,11 +44,11 @@ function App() {
             <Route exact path="/vet" element={<Veterinary />} />
             <Route exact path="/store/:id" element={<Productlisting />} />
             <Route exact path="/vet/:id" element={<Booking />} />
-            <Route exact path="/confirmBooking" element={<ConfirmBooking />} />
+            <Route exact path="/payment" element={<ConfirmBooking />} />
           </Routes>
         </BrowserRouter>
       </UserAuthContextProvider>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
