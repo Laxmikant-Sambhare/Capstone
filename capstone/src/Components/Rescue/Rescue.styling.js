e2f7db7da373f10e08f8f98643b40c452735c048
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-16 13:34:00 +0530

    Rescue.styling.js

--- capstone/src/Components/Rescue/Rescue.styling.js
+++ capstone/src/Components/Rescue/Rescue.styling.js
@@ -1,62 +1,62 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const Form = styled.div`
-  width: 35%;
-  height: 30%;
+// export const Form = styled.div`
+  // width: 35%;
+  // height: 30%;
   
-  border-radius:15px;
-  margin-top: -30%;
-  margin-left:32%;
-  /* margin-right: 40%;  */
-  /* padding: 2%; */
-  background-color:#ffd301;
+//   border-radius:15px;
+//   margin-top: -30%;
+//   margin-left:32%;
+//   /* margin-right: 40%;  */
+//   /* padding: 2%; */
+//   background-color:#ffd301;
   
-`;
-export const Container1 = styled.div`
-  border-radius:10px;
-  margin-left: 30%;
-  margin-top: -40%;
+// `;
+// export const Container1 = styled.div`
+//   border-radius:10px;
+//   margin-left: 30%;
+//   margin-top: -40%;
 
-  > input {
-    width: 50%;
-    margin-bottom: 5%;
-  }
-`;
-export const Container2 = styled.div`
-  border-radius:10px;
-  margin-left: 30%;
+//   > input {
+//     width: 50%;
+//     margin-bottom: 5%;
+//   }
+// `;
+// export const Container2 = styled.div`
+//   border-radius:10px;
+//   margin-left: 30%;
 
-  > input {
-    width: 48%;
-    margin-bottom: 5%;
-  }
-`;
-export const Container3 = styled.div`
-  border-radius:10px;
-  margin-left: 30%;
-  > input {
-    margin-bottom: 5%;
-    width: 51%;
-  }
-`;
-export const Container4 = styled.div`
-  border-radius:10px;
-  margin-left: 30%;
-  > input {
-    width: 55%;
-    margin-bottom: 5%;
-  }
-`;
-export const Container5 = styled.div`
-  border-radius:10px;
-  margin-left: 40%;
-  margin-bottom:5%;
-`;
-export const Button = styled.button`
-  width: 11%;
-  padding: 1%;
-  margin-left:45%;
-  margin-top:-50px;
-  /* background-color:black; */
-  border-radius:8px;
-`;
+//   > input {
+//     width: 48%;
+//     margin-bottom: 5%;
+//   }
+// `;
+// export const Container3 = styled.div`
+//   border-radius:10px;
+//   margin-left: 30%;
+//   > input {
+//     margin-bottom: 5%;
+//     width: 51%;
+//   }
+// `;
+// export const Container4 = styled.div`
+//   border-radius:10px;
+//   margin-left: 30%;
+//   > input {
+//     width: 55%;
+//     margin-bottom: 5%;
+//   }
+// `;
+// export const Container5 = styled.div`
+//   border-radius:10px;
+//   margin-left: 40%;
+//   margin-bottom:5%;
+// `;
+// export const Button = styled.button`
+//   width: 11%;
+//   padding: 1%;
+//   margin-left:45%;
+//   margin-top:-50px;
+//   /* background-color:black; */
+//   border-radius:8px;
+// `;
