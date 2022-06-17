581ee9095b050061102926217d3999a33e3965c2
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-17 09:52:52 +0530

    component

--- capstone/src/Components/Home/Home.js
+++ capstone/src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../../context/UserAuthContext";
 import styled from "styled-components";
-import { Slide } from 'react-slideshow-image';
+import { Slide } from "react-slideshow-image";
 import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 
@@ -21,49 +21,49 @@ const Home = () => {
   };
   const slideImages = [
     {
-      url: 'https://www.seekpng.com/png/full/3-39193_happy-birthday-if-you-have-clicked-through-to.png',
-      caption: 'Slide 1'
+      url: "https://www.seekpng.com/png/full/3-39193_happy-birthday-if-you-have-clicked-through-to.png",
+      caption: "Slide 1",
     },
     {
-      url: 'https://thumbs.dreamstime.com/b/collage-different-dogs-white-background-banner-design-179886014.jpg',
-      caption: 'Slide 2'
+      url: "https://thumbs.dreamstime.com/b/collage-different-dogs-white-background-banner-design-179886014.jpg",
+      caption: "Slide 2",
     },
     {
-      url: 'https://image.shutterstock.com/image-photo/collage-different-dogs-on-white-260nw-1724568655.jpg',
-      caption: 'Slide 3'
+      url: "https://image.shutterstock.com/image-photo/collage-different-dogs-on-white-260nw-1724568655.jpg",
+      caption: "Slide 3",
     },
     {
-      url: 'https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/various-dogs-horizontal-web-banner-susan-schmitz.jpg',
-      caption: 'Slide 4'
+      url: "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/various-dogs-horizontal-web-banner-susan-schmitz.jpg",
+      caption: "Slide 4",
     },
   ];
 
   const services = [
     {
-      id: 1 ,
-      url:'https://cdn.discordapp.com/attachments/947918952338706462/985164374740189244/unknown.png',
-      service : "Rescue",
-      path: "/rescue"
+      id: 1,
+      url: "https://cdn.discordapp.com/attachments/947918952338706462/985164374740189244/unknown.png",
+      service: "Rescue",
+      path: "/rescue",
     },
     {
-      id: 2 ,
-      url:'https://cdn.discordapp.com/attachments/947918952338706462/985170895230337056/unknown.png',
-      service : "Adoption",
-      path: "/adoption"
+      id: 2,
+      url: "https://cdn.discordapp.com/attachments/947918952338706462/985170895230337056/unknown.png",
+      service: "Adoption",
+      path: "/adoption",
     },
     {
-      id: 3 ,
-      url:'https://cdn.discordapp.com/attachments/947918952338706462/985172816485830686/unknown.png',
-      service : "Veterinary",
-      path: "/vet"
+      id: 3,
+      url: "https://cdn.discordapp.com/attachments/947918952338706462/985172816485830686/unknown.png",
+      service: "Veterinary",
+      path: "/vet",
     },
     {
-      id: 4 ,
-      url:'https://cdn.discordapp.com/attachments/947918952338706462/985174308450103316/unknown.png',
-      service : "Store",
-      path: "/store"
-    }
-  ]
+      id: 4,
+      url: "https://cdn.discordapp.com/attachments/947918952338706462/985174308450103316/unknown.png",
+      service: "Store",
+      path: "/store",
+    },
+  ];
   return (
     <>
       <div className="p-4 box mt-3 text-center">
@@ -76,20 +76,28 @@ const Home = () => {
         </Button>
       </div>
       <div>
-      <Banner>
-      <div className="slide-container">
-        <Slide>
-         {slideImages.map((slideImage, index)=> (
-            <div className="each-slide" key={index} style={{alignItems: 'center'}}>
-           <img src={slideImage.url} alt="Category"  style={{
-                   height: '350px',
-                   width: '1400px'
-                  }}/>  
-            </div>
-          ))} 
-        </Slide>
-      </div>
-      </Banner>
+        <Banner>
+          <div className="slide-container">
+            <Slide>
+              {slideImages.map((slideImage, index) => (
+                <div
+                  className="each-slide"
+                  key={index}
+                  style={{ alignItems: "center" }}
+                >
+                  <img
+                    src={slideImage.url}
+                    alt="Category"
+                    style={{
+                      height: "350px",
+                      width: "1400px",
+                    }}
+                  />
+                </div>
+              ))}
+            </Slide>
+          </div>
+        </Banner>
         <div
           style={{
             display: "flex",
@@ -100,36 +108,36 @@ const Home = () => {
             textAlign: "center",
           }}
         >
-             <Categories>
-        {services.map((data, index) => {
-          return (
-            <Link to={data.path} key={index}>
-              <Catergory>
-                <img src={data.url} alt="Category" />
-                <Typography
-                  style={{
-                    textAlign: "center",
-                    fontSize: 20,
-                    fontWeight: 600,
-                    color: "black",
-                  }}
-                >
-                  {data.service}
-                </Typography>
-              </Catergory>
-            </Link>
-          );
-        })}
-      </Categories>
+          <Categories>
+            {services.map((data, index) => {
+              return (
+                <Link to={data.path} key={index}>
+                  <Catergory>
+                    <img src={data.url} alt="Category" />
+                    <Typography
+                      style={{
+                        textAlign: "center",
+                        fontSize: 20,
+                        fontWeight: 600,
+                        color: "black",
+                      }}
+                    >
+                      {data.service}
+                    </Typography>
+                  </Catergory>
+                </Link>
+              );
+            })}
+          </Categories>
         </div>
-        <Bottomtabs/>
+        <Bottomtabs />
       </div>
     </>
   );
 };
 const Bottomtabs = styled.div`
-height:50px;
-`
+  height: 50px;
+`;
 const Categories = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -151,10 +159,10 @@ const Catergory = styled.div`
   }
 `;
 const Banner = styled.div`
-align-items: center;
-margin-left:80px;
-margin-right:80px;
- height: 350px;
+  align-items: center;
+  margin-left: 80px;
+  margin-right: 80px;
+  height: 350px;
 `;
 
 export default Home;
