698c0fe778fee90ffe8c6d679d36a14e55fd6950
nitta sreenivas <nitta.sreenivas@gmail.com>
2022-06-17 11:33:32 +0530

    add desc

--- capstone/src/Components/Adoption/DogDescription.js
+++ capstone/src/Components/Adoption/DogDescription.js
@@ -1,63 +1,20 @@
-<<<<<<< HEAD
-import { useParams } from "react-router-dom";
-=======
 
-import { useParams} from "react-router-dom";
-import { Navigate } from "react-router-dom";
-import { useState } from "react";
->>>>>>> nitta-New
-import data from "./data.json";
-import React from "react";
+import { useParams } from "react-router-dom";
+import data from "../Adoption/data";
 export default function DogDescription() {
-  const [adopt,setAdopt] = useState(false);
-  // if(adopt) {
-  //   return <Navigate to = "/signin"></Navigate>
-  // }
-  
   const params = useParams();
   console.log("Params", params);
   const doggie = data.find((dog) => dog.id === params.id);
-  if(adopt) {
-    return <Navigate to = "/forms"/>
-  }
   return (
     <div className="dog-information">
       <h1> ABOUT DOG.. </h1>
-      <h3>
-        {" "}
-        <em> Breed: </em> {doggie.Breed}{" "}
-      </h3>
-      <img alt="Dog" src={doggie.image} width="300" />
-<<<<<<< HEAD
-      <h3>
-        {" "}
-        <em> Description:</em> {doggie.description}{" "}
-      </h3>
-      <h3>
-        {" "}
-        <em>Color:</em> {doggie.Color}{" "}
-      </h3>
-      <h3>
-        <em>Rescued From:</em> {doggie["Rescued From"]}{" "}
-      </h3>
-      <h3>
-        {" "}
-        <em>Life Expectancy:</em> {doggie["Life Expectancy"]}{" "}
-      </h3>
-      <h3>
-        {" "}
-        <em> Approx Age:</em> {doggie["Approx Age"]}{" "}
-      </h3>
-=======
-      <h3> <em> Description:</em> {doggie.description} </h3>
-      <h3> <em>Color:</em> {doggie.Color} </h3>
-      <h3><em>Rescued From:</em> {doggie["Rescued From"]} </h3>
-      <h3> <em>Life Expectancy:</em> {doggie["Life Expectancy"]} </h3>
-      <h3> <em> Approx Age:</em> {doggie["Approx Age"]} </h3>
-      <br/>
-      <br/>
-     <button className = "button" onClick = {() => setAdopt(true)}> ADOPT</button>
->>>>>>> nitta-New
+      <h3> {doggie.Breed} </h3>
+      <img alt="Dog" src={doggie.image} width="500" />
+      <h3> {doggie.description} </h3>
+      <h3> {doggie.Color} </h3>
+      <h3> {doggie["Rescued From"]} </h3>
+      <h3> {doggie["Life Expectancy"]} </h3>
+      <h3> {doggie["Approx Age"]} </h3>
     </div>
   );
 }
