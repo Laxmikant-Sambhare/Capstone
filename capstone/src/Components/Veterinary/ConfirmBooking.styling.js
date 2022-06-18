f5d8b1c33ac2a4bedf3bd4ffde7a55adf05d8b50
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-18 09:29:18 +0530

    styling

--- capstone/src/Components/Veterinary/ConfirmBooking.styling.js
+++ capstone/src/Components/Veterinary/ConfirmBooking.styling.js
@@ -1,35 +1,16 @@
 import styled from "styled-components";
-export const Bookings = styled.div`
-  width: 400px;
-  height: 500px;
-  border-color: black;
-  border-width: 1px;
-  border-style: solid;
-  border-radius: 10%;
-  position: absolute;
-  margin-top: 8%;
-  margin-left: 40%;
-  box-shadow: 0 4px 8px 0 #f5ac6e, 0 6px 20px 0 #f5ac6e;
-`;
-export const Containers = styled.div`
-  display: flex;
 
-  flex-direction: wrap;
-
+export const Text = styled.h2`
   justify-content: center;
 `;
-export const HeadingTag = styled.div`
-  text-align: center;
-  font-weight: bolder;
-  font-size: 30px;
-  margin-top: 5%;
-`;
-export const Text = styled.h1``;
-export const PaymentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 5%;
-  font-weight: 700;
-  margin-top: 5%;
-  text-align: center;
+export const Form = styled.div`
+  width: 40%;
+  height: 700px;
+  font-size: 14px;
+  /* background-color: #ffd301; */
+  margin-top: 3%;
+  margin-left: 31%;
+  padding: 2%;
+  border-radius: 15px;
+  /* border: 1px solid #000; */
 `;
