e2aaf8df1317e2e388d054e4b7c950af7b200c96
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-18 09:29:38 +0530

    styling

---
+++ capstone/src/Components/Veterinary/Payment.styling.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+export const Bookings = styled.div`
+  width: 400px;
+  height: 500px;
+  border-color: black;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 10%;
+  position: absolute;
+  margin-top: 8%;
+  margin-left: 40%;
+  box-shadow: 0 4px 8px 0 #f5ac6e, 0 6px 20px 0 #f5ac6e;
+`;
+export const Containers = styled.div`
+  display: flex;
+
+  flex-direction: wrap;
+
+  justify-content: center;
+`;
+export const HeadingTag = styled.div`
+  text-align: center;
+  font-weight: bolder;
+  font-size: 30px;
+  margin-top: 5%;
+`;
+export const Text = styled.h1``;
+export const PaymentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+  font-weight: 700;
+  margin-top: 5%;
+  text-align: center;
+`;
