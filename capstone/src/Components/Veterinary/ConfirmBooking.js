0f52e8647b87013b9d586a3a7378707ee85063fd
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-20 00:47:56 +0530

    component

--- capstone/src/Components/Veterinary/ConfirmBooking.js
+++ capstone/src/Components/Veterinary/ConfirmBooking.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Styling.css";
-import { Text1, Form } from "./Styling";
+import { Text1 } from "./Styling";
 import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,18 +12,17 @@ function ConfirmBooking() {
   //   Object.keys(formErrors).length === 0 && isSubmit && navigate("/payment");
   //   // <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
   // };
-  const { bookingItems } = useSelector((state) => state.bookings)
+  const { bookingItems } = useSelector((state) => state.bookings);
   const location = useLocation();
   const Bookingdata = location.state;
-  const dispatch =  useDispatch();
+  const dispatch = useDispatch();
   const [isBooked, setIsBooked] = useState(false);
-  const handleAddtoBookings = (formValues,) => {
-    if (Object.keys(formErrors).length === 0 && isSubmit){
-    dispatch(addTOBookings(formValues))
-    setIsBooked(true);
+  const handleAddtoBookings = (formValues) => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      dispatch(addTOBookings(formValues));
+      setIsBooked(true);
     }
-    
-  }
+  };
 
   const initialValues = {
     id: `${bookingItems.length}`,
@@ -36,12 +35,10 @@ function ConfirmBooking() {
     Service: `${Bookingdata.service}`,
     Image: `${Bookingdata.image1}`,
     price: `${Bookingdata.price}`,
-
   };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
-  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -92,131 +89,142 @@ function ConfirmBooking() {
     return errors;
   };
   return (
-    <div style={{ marginBottom: "100px" }} className="BackgroundImage">
-      <Form className="form">
+    <div className="BackgroundImage">
+      <div style={{ margin: "30px " }}>
         <div className="container">
           <form onSubmit={handleSubmit}>
-            <div className="ui divider"></div>
             <div className="ui form">
-              <div style={{ backgroundColor: "#ffd301" }}>
-                <img
+              <div
+                style={{
+                  backgroundColor: "#ffd301",
+                  width: "100%",
+                  borderRadius: "10px 10px 0 0",
+                }}
+              >
+                {/* <img
                   src={require("./complete-small.jpg")}
                   alt="logo"
                   className="Mainname"
                   style={{ height: "5rem", padding: "2px" }}
-                />
-                <Text1
-                  style={{
-                    marginTop: "-35px",
-                    marginBottom: "15px",
-                    marginLeft: "60px",
-                  }}
-                >
-                  BOOK YOUR SLOTS
+                /> */}
+                <Text1>
+                  <h3>{Bookingdata.service}</h3>
                 </Text1>
               </div>
-              <h3>Service : {Bookingdata.service}</h3>
-              <div className="fields">
-                <label>FirstName</label>
-                <input
-                  type="text"
-                  name="FirstName"
-                  placeholder=" Enter your Firstname"
-                  value={formValues.FirstName}
-                  onChange={handleChange}
-                  style={{ marginLeft: "30px" }}
-                />
-                <p className="para">{formErrors.FirstName}</p>
-              </div>
+              <div
+                style={{
+                  padding: "20px",
+                }}
+              >
+                <div className="bookingRow">
+                  <div className="fields">
+                    <input
+                      type="text"
+                      name="FirstName"
+                      placeholder=" Enter your Firstname"
+                      value={formValues.FirstName}
+                      onChange={handleChange}
+                    />
+                    <p className="para">{formErrors.FirstName}</p>
+                  </div>
 
-              <div className="fields">
-                <label>LastName</label>
-                <input
-                  type="text"
-                  name="LastName"
-                  placeholder="Enter Your Lastname"
-                  value={formValues.LastName}
-                  onChange={handleChange}
-                  style={{ marginLeft: "34px" }}
-                />
-                <p className="para">{formErrors.LastName}</p>
-              </div>
+                  <div className="fields">
+                    <input
+                      type="text"
+                      name="LastName"
+                      placeholder="Enter Your Lastname"
+                      value={formValues.LastName}
+                      onChange={handleChange}
+                    />
+                    <p className="para">{formErrors.LastName}</p>
+                  </div>
+                </div>
+                <div className="bookingRow">
+                  <div className="fields">
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder="Enter Your Email"
+                      value={formValues.email}
+                      onChange={handleChange}
+                    />
+                    <p className="para">{formErrors.email}</p>
+                  </div>
+                  <div className="fields">
+                    <input
+                      type="string"
+                      name="ContactNo"
+                      placeholder="Enter Your Contact Number"
+                      value={formValues.ContactNo}
+                      onChange={handleChange}
+                    />
+                    <p className="para">{formErrors.ContactNo}</p>
+                  </div>
+                </div>
+                <div className="bookingRow">
+                  <div className="fields">
+                    <input
+                      type="text"
+                      name="Address"
+                      placeholder="Enter Your Address"
+                      value={formValues.Address}
+                      onChange={handleChange}
+                    />
+                    <p className="para">{formErrors.Address}</p>
+                  </div>
 
-              <div className="fields">
-                <label>Email</label>
-                <input
-                  type="text"
-                  name="email"
-                  placeholder="Enter Your Email"
-                  value={formValues.email}
-                  onChange={handleChange}
-                  style={{ marginLeft: "70px" }}
-                />
-                <p className="para">{formErrors.email}</p>
+                  <div className="fields">
+                    <select
+                      style={{ padding: "10px", width: "100%" }}
+                      name="slots"
+                      onChange={handleChange}
+                      value={formValues.slots}
+                    >
+                      <option value="">Slots-Available</option>
+                      <option value="12am-2am">12am-2am</option>
+                      <option value="2am-4am">2am-4am</option>
+                      <option value="4am-6am">4am-6am</option>
+                      <option value="6am-8am">6am-8am</option>
+                      <option value="8am-10am">8am-10am</option>
+                      <option value="10am-12pm">10am-12pm</option>
+                      <option value="12pm-2pm">12pm-2pm</option>
+                      <option value="2pm-4pm">2pm-4pm</option>
+                      <option value="4pm-6pm">4pm-6pm</option>
+                      <option value="6pm-8pm">6pm-8pm</option>
+                      <option value="8pm-10pm">8pm-10pm</option>
+                      <option value="10pm-12am">10pm-12am</option>
+                    </select>
+                  </div>
+                </div>
+                <p style={{ fontSize: "15px" }}>
+                  T & C* <br></br>A service charge of X will be charged from the
+                  user. This amount will not be refunded under any
+                  circumstances.
+                </p>
               </div>
-              <div className="fields">
-                <label>ContactNo</label>
-                <input
-                  type="string"
-                  name="ContactNo"
-                  placeholder="Enter Your Contact Number"
-                  value={formValues.ContactNo}
-                  onChange={handleChange}
-                  style={{ marginLeft: "29px" }}
-                />
-                <p className="para">{formErrors.ContactNo}</p>
-              </div>
-
-              <div className="fields">
-                <label>Address</label>
-                <input
-                  type="text"
-                  name="Address"
-                  placeholder="Enter Your Address"
-                  value={formValues.Address}
-                  onChange={handleChange}
-                  style={{ marginLeft: "48px" }}
-                />
-                <p className="para">{formErrors.Address}</p>
-              </div>
-
-              <div className="fields">
-                <label>Slots Available</label>
-                <select 
-                  name="slots"
-                  onChange={handleChange}
-                  value={formValues.slots}
-                >
-                  <option value="">None</option>
-                  <option value="12am-2am">12am-2am</option>
-                  <option value="2am-4am">2am-4am</option>
-                  <option value="4am-6am">4am-6am</option>
-                  <option value="6am-8am">6am-8am</option>
-                  <option value="8am-10am">8am-10am</option>
-                  <option value="10am-12pm">10am-12pm</option>
-                  <option value="12pm-2pm">12pm-2pm</option>
-                  <option value="2pm-4pm">2pm-4pm</option>
-                  <option value="4pm-6pm">4pm-6pm</option>
-                  <option value="6pm-8pm">6pm-8pm</option>
-                  <option value="8pm-10pm">8pm-10pm</option>
-                  <option value="10pm-12am">10pm-12am</option>
-                </select>
-              </div>
-              <p style={{ fontSize: "15px" }}>
-                T & C* <br></br>A service charge of X will be charged from the
-                user. This amount will not be refunded under any circumstances.
-              </p>
             </div>
-          
-          { (isBooked !== true)?<button className="Submit" onClick={() => handleAddtoBookings(formValues)}>Book Your Slots</button>:
-            <Link to={'/payment'} state={Bookingdata}>
-            <button className="Paymentbtn" style={{ backgroundColor: "red" }}>
-              Continue To Payment
-            </button>
-            </Link>}
+            <div className="bookYourSlots">
+              {isBooked !== true ? (
+                <button
+                  className="Submit"
+                  onClick={() => handleAddtoBookings(formValues)}
+                >
+                  Book Your Slots
+                </button>
+              ) : (
+                <Link to={"/payment"} state={Bookingdata}>
+                  <button
+                    className="Paymentbtn"
+                    style={{ backgroundColor: "red" }}
+                  >
+                    Continue To Payment
+                  </button>
+                </Link>
+              )}
+            </div>
           </form>
         </div>
-      </Form>
+      </div>
     </div>
   );
 }
