57e0fc4d6aa9299b2ffd2c684247d235754411fe
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-06-21 10:45:09 +0530

    data

--- capstone/src/Components/Veterinary/Data/Services.js
+++ capstone/src/Components/Veterinary/Data/Services.js
@@ -3,53 +3,55 @@ export const Services = [
     id: 1,
     service: "Emergency Ambulance",
     Description:
-      "Our Veterinary Clinic Mobile Offers 24 Hour Emergency Vet Care and Emergency ambulance service, As Well as we provide routine Veterinary Services For Your Pets. Our Exclusive Emergency Vet Clinic Near You is Known To Be The Best dog vet, Large dog vet, And Urgent Vet Clinic in Mobile To Treat Your dog With Extra Kindness. Our Veterinary Clinic Mobile's emergency specialists and employees are focused toward Emergency Medicine To Cure Your dog in the most dangerous circumstances. Our emergency team is led by the best employees in Mobile. Our certified veterinary specialists are highly experienced and can offer immediate treatment in serious cases. We have the largest group of emergency Veterinary Care Doctors in Mobile. Following your emergency, we connect you with a veterinarian to ensure your pet continues to recover properly.",
+      "Our Veterinary Clinic Mobile Offers 24 Hour Emergency Vet Care and Emergency ambulance service, As Well as we provide routine Veterinary Services For Your Dogs. Our Exclusive Emergency Vet Clinic Near You is Known To Be The Best dog vet, Large dog vet, And Urgent Vet Clinic in Mobile To Treat Your dog With Extra Kindness. Our Veterinary Clinic Mobile's emergency specialists and employees are focused toward Emergency Medicine To Cure Your dog in the most dangerous circumstances. Our emergency team is led by the best employees in Mobile. Our certified veterinary specialists are highly experienced and can offer immediate treatment in serious cases. We have the largest group of emergency Veterinary Care Doctors in Mobile. Following your emergency, we connect you with a veterinarian to ensure your dog continues to recover properly.",
     image1: require("../images/ambulance1.png"),
     image: require("../images/Emergency-ambulance.jpg"),
-    price: '250'
+    price: "250",
   },
   {
     id: 2,
     service: "Consultation",
     Description:
-      "Our team provide 24*7 hours consultation for dogs.During your dog’s consultation, we will take a history and discuss with you your dog’s symptoms. Your dog will be examined by the vet, looking at key areas of concern but also taking into account a wider overview of their body systems. For larger dogs, this usually takes place on the floor and for smaller pets, on the consulting table. We feel that you and your pet deserve not to feel rushed during the consultation, so all our appointments are at least 15 minutes long. If you are seeing our specialist this will take longer, so please take this into account when arranging your appointment.To assist us in diagnosing your dog’s problems it is always a great help if any records you have of changes in their normal routine. Videos of unusual behaviour and, in the case of any urinary problems, a sample is always appreciated! If your dog is on medication, or supplements we don’t know about, it is helpful if you have these records with you. If you think your pet has ingested a toxic substance, keeping that packaging can speed up identifying the correct treatment.",
-    image1: require("../images/consulytation3.jpg"),
+      "Our team provide 24*7 hours consultation for dogs. During your dog’s consultation, we will take a history and discuss with you your dog’s symptoms. Your dog will be examined by the vet, looking at key areas of concern but also taking into account a wider overview of their body systems. For larger dogs, this usually takes place on the floor and for smaller pets, on the consulting table. We feel that you and your dog deserve not to feel rushed during the consultation, so all our appointments are at least 15 minutes long. If you are seeing our specialist this will take longer, so please take this into account when arranging your appointment.To assist us in diagnosing your dog’s problems it is always a great help if any records you have of changes in their normal routine. Videos of unusual behaviour and, in the case of any urinary problems, a sample is always appreciated! If your dog is on medication, or supplements we don’t know about, it is helpful if you have these records with you. If you think your dog has ingested a toxic substance, keeping that packaging can speed up identifying the correct treatment.",
+    image1: require("../images/WellnessAndPreventivecare1.jpg"),
     image: require("../images/consultation.png"),
-    price: '250'
+    price: "250",
   },
   {
     id: 3,
     service: "Grooming",
-    Description: "Our services includes dog grooming.",
+    Description:
+      "our Mobile Grooming is the best dog-friendly grooming service for any breed. Our fully-equipped dog service van can reach any place you want and at any time you want. Our team use good quality grooming products and are very transparent about their products and process. Our Salon is equipped with hot & cold running water for a comfortable bathing experience, it is also fully air-conditioned keeping your pet’s comfort in mind, full sized tub and a state of the art grooming table. We provide personal uninterrupted attention for you and your pet in a quite and stress free environment.",
     image1: require("../images/dog-grooming2.png"),
     image: require("../images/dog-grooming.jpg"),
-    price: '250'
+    price: "250",
   },
   {
     id: 4,
     service: "Dog Training",
-    Description: "Our services includes dog training.",
+    Description:
+      "Your dog deserves the very best training!. Does your dog run your household? Do you wish your dog could join your family on public outings or calmly socialize with visiting guests? That isn't a dream, and it can quickly become a reality! Our training packages are designed to help you regain control of your life and set you on a path of success with responsible dog ownership and a well-behaved canine citizen you can be proud of. Our dog gtraining services are available round the clock with Puppy Trained Right, you'll learn how to create an environment conducive to a smooth transition, so that your puppy can adapt easily to your home. Begin training your puppy now so that you can enjoy a well balanced, obedient puppy for many years to come!",
     image1: require("../images/dog-training2.jpg"),
     image: require("../images/dog-training.jpg"),
-    price: '250'
+    price: "250",
   },
   {
     id: 5,
     service: "Dog Dentistry",
     Description:
-      "Get Suitable Dental Care At Home That Consists of Daily Brushing. Our Dog Dentist in Mobile Is The Best Dentist For Dogs in Your Area. At Our Puppy Dental Care Clinic, Our Staff Will Teach You How To Brush Your Dog's Teeth And Offer The Best Brush And Dental Paste According To Your Dog's Size. It is important to understand that your dog will never express when it is experiencing dental pain and discomfort. All dogs will endure some level of Dental Ailment And Dental Pain. The optimum way to reduce these problems is for your dog to start out with a consistent dental health routine at home. This should be paired with regular dental cleanings done by a veterinary expert. Proper Dental Care At Home is necessary. Our Veterinary Clinic Mobile are here to teach you how to brush your dog's teeth and offer you the best brush and dental paste according to your dog's size. Veterinary Dental Services in Mobile. Dental treatment is important to the health of any animal. Issues with the heart, liver, and kidneys can be a result of very poor oral hygiene. In spite of these worrisome facts, many pet owners are not aware that their dog's mouth says alot about it's health. The most effective method to Protect Your dog From Dental Issues is through professional cleanings at our Mobile Veterinary Clinic. Our Mobile Veterinary Clinic will give thorough cleanings, including the space underneath the gums that you can't see or reach at home. 'Mobile Dog Dental Treatment' The dog dental care services offered here is an analysis by one of our veterinarians. If dental problems are located in your dog's mouth, our Dog Veterinarian will describe the circumstance to you and suggest the right dental process. During this procedure, once your dog is anesthetized, our veterinarians are able to conduct a more thorough exam of the dog's mouth. Our Veterinary Clinic Mobile uses all the dental tools needed to make sure your Dog Is Fully Free of Dental Issues and pain. 'Anesthetic Dentistry in Mobile ' Not all procedures can be done anesthesia-free. But some dog Dental Procedures can be done without using general anesthesia. General anesthesia allows us to carefully assess the Dog's Oral Health and peform the required processes while they are safely sedated.",
+      "Get Suitable Dental Care At Home That Consists of Daily Brushing. Our Dog Dentist in Mobile Is The Best Dentist For Dogs in Your Area. At Our dental Care Mobile, Our Staff Will Teach You How To Brush Your Dog's Teeth And Offer The Best Brush And Dental Paste According To Your Dog's Size. It is important to understand that your dog will never express when it is experiencing dental pain and discomfort. All dogs will endure some level of Dental Ailment And Dental Pain. The optimum way to reduce these problems is for your dog to start out with a consistent dental health routine at home. This should be paired with regular dental cleanings done by a veterinary expert. Proper Dental Care At Home is necessary.  Dental treatment is important to the health of any animal. Issues with the heart, liver, and kidneys can be a result of very poor oral hygiene. In spite of these worrisome facts, many pet owners are not aware that their dog's mouth says alot about it's health. The most effective method to Protect Your dog From Dental Issues is through professional cleanings at our Mobile Veterinary Clinic. Our Mobile Veterinary Clinic will give thorough cleanings, including the space underneath the gums that you can't see or reach at home. ",
     image1: require("../images/dental5.jpg"),
     image: require("../images/dentistry.jpg"),
-    price: '250'
+    price: "250",
   },
   {
     id: 6,
     service: "Mobile Veterinary",
     Description:
-      "Our Veterinary Clinic Mobile's emergency specialists and employees are focused toward Emergency Medicine To Cure Your dog in the most dangerous circumstances. Our emergency team is led by the best employees in Mobile. Our certified veterinary specialists are highly experienced and can offer immediate treatment in serious cases. We have the largest group of emergency Veterinary Care Doctors in Mobile. Following your emergency, we connect you with a veterinarian to ensure your pet continues to recover properly.",
+      "Our Veterinary Clinic Mobile's emergency specialists and employees are focused toward Emergency Medicine To Cure Your dog in the most dangerous circumstances. Our emergency team is led by the best employees in Mobile. Our certified veterinary specialists are highly experienced and can offer immediate treatment in serious cases. We have the largest group of emergency Veterinary Care Doctors in Mobile. Following your emergency, we connect you with a veterinarian to ensure your pet continues to recover properly.It’s often difficult to bring large pets or injured or sick animals to the clinic. And it’s common that people prefer a vet to visit their houses rather than taking the animals to the clinic, and that’s been a system for long here in Kerala. Understanding this need of our clients, Felican Pet Hospital has introduce a mobile veterinary unit that’s equipped with all facilities to provide first aid and general treatment to your pets or domesticated animals.",
     image1: require("../images/vet-clinic1.jpg"),
     image: require("../images/veterinary.jpg"),
-    price: '250'
+    price: "250",
   },
   {
     id: 7,
@@ -58,7 +60,7 @@ export const Services = [
       "Protect Your Dogs From Deadly Diseases By Getting Our Dog Vaccination Services in Mobile. We Are A Reputable Clinic Providing Small Dog Vaccinations, Rabies Vaccines For Dogs, And Canine Vaccination Services Across Mobile For The Health of Your dog Dogs. Vaccines are very important for puppies and dogs. When you Bring Your dog To George Veterinary Clinic Mobile, we'll work with you to understand your pet's requirements and medical past before we recommend which vaccinations are optimal for your pet. Dog vaccinations play a crucial part in Protecting Your Dog from many hazardous and even lethal diseases. At George Veterinary Clinic Mobile we have spent decades instructing people about the benefits of dog vaccinations. We explain why vaccines are needed and how they should be scheduled. At George Veterinary Clinic Mobile your dog's upcoming veterinary appointment, we will be happy to help you to understand the Vaccinations For Your Dog.",
     image1: require("../images/dog_vaccination1.jpg"),
     image: require("../images/dog-vaccination.jpg"),
-    price: '250'
+    price: "250",
   },
   {
     id: 8,
@@ -67,7 +69,7 @@ export const Services = [
       "We finish your pet the numerous periods of their lives. From specific and deterrent consideration during their more youthful years to ordinary health checks as they age, we try to forestall and recognize issues before they emerge. We realize your pet is imperative to you, that is the reason we endeavor to guarantee they are given simply the best in consideration through all the long stretches of their lives. The American Animal Hospital Association suggests that your pet have a wellbeing test at any rate once consistently. As your pet ages, they suggest having considerably more. We accept firmly in preventive mind and have created wellbeing conventions for each phase of the life of your pet. Contingent upon your pet's life stage, antibodies might be managed, just as infection screenings.",
     image1: require("../images/wellness3.png"),
     image: require("../images/WellnessAndPreventivecare.jpg"),
-    price: '250'
+    price: "250",
   },
   // {
   //   id: 9,
