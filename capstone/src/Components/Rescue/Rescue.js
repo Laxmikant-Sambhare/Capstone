808c806459e8be25be7d434f079e2037a3b508f0
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-22 12:11:42 +0530

    Rescue.js

--- capstone/src/Components/Rescue/Rescue.js
+++ capstone/src/Components/Rescue/Rescue.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import Form from "./Form";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import React, { useState } from "react"
+import Form from "./Form"
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { storage } from "../../firebase";
 import { v4 } from "uuid";
 import { toast } from "react-toastify";
